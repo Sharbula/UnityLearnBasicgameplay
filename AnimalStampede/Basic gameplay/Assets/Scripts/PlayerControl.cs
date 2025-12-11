bc95f7e2091b3be28e11741552687b3d3c157928
sharbula <elhelous@roverkids.org>
2025-12-11 14:00:20 -0500

    2.2 done

--- AnimalStampede/Basic gameplay/Assets/Scripts/PlayerControl.cs
+++ AnimalStampede/Basic gameplay/Assets/Scripts/PlayerControl.cs
@@ -6,6 +6,8 @@ public class PlayerControl : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+
+    public GameObject projectilePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -27,6 +29,12 @@ public class PlayerControl : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }    
